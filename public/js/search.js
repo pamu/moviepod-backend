ce38e48b31d30be0659527dfacea9d386070f74f
pamu <p_nagarjuna@students.iitmandi.ac.in>
2016-04-17 15:28:46 +0530

    https

--- public/js/search.js
+++ public/js/search.js
@@ -3,7 +3,7 @@ $(function search() {
         if(e.which === 13) {
             var text = $("#search").val();
             if (text.length > 0) {
-                fetch('http://www.omdbapi.com/?s=' + text.trim() + '&page=1');
+                fetch('https://www.omdbapi.com/?s=' + text.trim() + '&page=1');
             }
         }
     });
