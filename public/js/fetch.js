d24aadc02d36fa16172240fad3292964199ee69b
pamu <p_nagarjuna@students.iitmandi.ac.in>
2016-04-17 16:01:58 +0530

    page reload stopped

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -1,8 +1,10 @@
 function fetch(url) {
     showProgress();
+    console.log("trying url " + url);
     $.get(url, function(data) {
         $("#movies").html('');
         var payload = data['Search'];
+        console.log(JSON.stringify(payload));
         for(var i = 0; i < payload.length; i ++) {
                 var movie = payload[i];
                 console.log(JSON.stringify(movie));
@@ -11,6 +13,7 @@ function fetch(url) {
     }).done(function(data) {
         //console.log(JSON.stringify(data));
     }).fail(function(error) {
+        console.log(JSON.stringify(error));
         $("#movies").html("error loading results, refresh to try again.");
     }).always(function() {
         console.log("network operation done");
--- public/js/search.js
+++ public/js/search.js
@@ -2,6 +2,7 @@ $(function search() {
     $("#search").keypress(function(e) {
         if(e.which === 13) {
             var text = $("#search").val();
+            console.log("enter");
             if (text.length > 0) {
                 fetch('https://www.omdbapi.com/?s=' + text.trim() + '&page=1');
             }
